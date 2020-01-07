5dcb4aaadd49ddd515953e15c9e13a58702318f9
maqi <maqi233968@163.com>
2020-01-07 10:25:46 +0800

    修改 注释

--- src/main/java/algorithms/NQueens.java
+++ src/main/java/algorithms/NQueens.java
@@ -1,8 +1,9 @@
 package algorithms;
 
 public class NQueens {
-
+    //4个皇后
     static final int N = 4;
+    //记录皇后位置
     protected static int[][] queens = new int[N][2];
     static void init(){
         for (int i=0;i<N;i++){
@@ -11,10 +12,22 @@ public class NQueens {
         }
     }
 
+    /**
+     * 获取指定皇后位置
+     * @param targetQueen 目标
+     * @return
+     */
     static int[] getQueenPos(int targetQueen){
         return queens[targetQueen];
     }
 
+    /**
+     * 判断皇后周围是否有其他皇后
+     * @param row
+     * @param col
+     * @param targetQueen
+     * @return
+     */
     static boolean valideQueen(int row, int col, int targetQueen){
         for (int i=0;i<targetQueen;i++){
             int[] qPos = getQueenPos(i);
@@ -48,6 +61,8 @@ public class NQueens {
 
     public static void main(String[] args) {
         nQ(0,0);
+
+        //打印出皇后位置图
         for (int i=0;i<N;i++){
             for (int j=0;j<N;j++){
                 boolean flag = false;

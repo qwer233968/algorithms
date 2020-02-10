ad8756fbabab19aa2e09f5e8ffaad10f92abf6b2
maqi <maqi233968@163.com>
2020-02-10 11:47:54 +0800

    修改 注释

--- src/main/java/map/MapTest.java
+++ src/main/java/map/MapTest.java
@@ -1,14 +1,12 @@
 package map;
 
-import java.io.File;
-
 public class MapTest {
 
     public static void main(String[] args) {
         HashMapDemo map = new HashMapDemo(133, 0.75f);
-        String s = "test";
-        map.put(s,"123");
-        map.put(s,"456");
-        System.out.println("test");
+        //String s = "test";
+        map.put("123","123");
+        map.put("456","456");
+        map.put("789","789");
     }
 }

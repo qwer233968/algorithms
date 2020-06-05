1506309c0565cefd131e5a18f044f5d8f8a12764
maqi <maqi233968@163.com>
2020-06-05 17:14:44 +0800

    顺时针矩阵

--- src/main/java/algorithms/leetcode/NumberDemo.java
+++ src/main/java/algorithms/leetcode/NumberDemo.java
@@ -170,17 +170,33 @@ public class NumberDemo {
         }
     }*/
 
-    public int getMaxNumberArray(int[] nums, int i, int j, int k, int l){
-        int len = nums.length;
-        int val1 = nums[i],val2 = 0,val3 = 0,val4 = 0;
-        if (j<len) val2 = nums[j];
-        if (k<len) val3 = nums[k];
-        if (l<len) val4 = nums[l];
-        int max1 = val1+val3;
-        int max =  Math.max(max1, val2+val4);
-        if (max == max1) return val1;
-        else return val2;
+    /**
+     * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+     *
+     * 示例 1：
+     *
+     * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * 输出：[1,2,3,6,9,8,7,4,5]
+     * @param matrix
+     * @return
+     */
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix.length == 0) return new int[0];
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while(true) {
+            for(int i = l; i <= r; i++) res[x++] = matrix[t][i]; // left to right.
+            if(++t > b) break;
+            for(int i = t; i <= b; i++) res[x++] = matrix[i][r]; // top to bottom.
+            if(l > --r) break;
+            for(int i = r; i >= l; i--) res[x++] = matrix[b][i]; // right to left.
+            if(t > --b) break;
+            for(int i = b; i >= t; i--) res[x++] = matrix[i][l]; // bottom to top.
+            if(++l > r) break;
+        }
+        return res;
     }
+
     public static void main(String[] args) {
         /*boolean is = isHappy(19);
         System.out.println(is);*/
@@ -189,7 +205,10 @@ public class NumberDemo {
         NumberDemo demo = new NumberDemo();
         /*String s = demo.longestPalindrome("babaabad");
         System.out.println(s);*/
-        int res = demo.findDuplicate(new int[]{3,1,4,2,3});
+        /*int res = demo.findDuplicate(new int[]{3,1,4,2,3});
+        System.out.println(res);*/
+        //int[] res = demo.spiralOrder(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}});
+        int[] res = demo.spiralOrder(new int[][]{{7},{9},{6}});
         System.out.println(res);
     }
 }
